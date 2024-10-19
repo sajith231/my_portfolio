a233620e373bb8f4465cfb4a3dd1dd653103521d
sajith231 <sajiththomas231@gmail.com>
2024-10-19 17:27:16 +0530

    skshhjd

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+!0i&c=sx4!=sgjfioco4$q%m9vo3p^m38umus_75e=$o*cxaa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','13.233.159.21']
+ALLOWED_HOSTS = ['127.0.0.1','13.232.125.157']
 
 
 # Application definition
